99bd8a0c3711f764fc09d926b75f176a17570ea9
SifEddine05 <sifousamir72@gmail.com>
2024-10-06 23:06:53 +0100

    make all pages responsive

--- Admin-dashboard/src/components/SideBar.jsx
+++ Admin-dashboard/src/components/SideBar.jsx
@@ -1,50 +1,75 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; // Import Link for navigation
 import { BiHome, BiCalendar, BiShoppingBag, BiBook, BiStats } from 'react-icons/bi'; // Import necessary icons
+import { IoMdCloseCircle } from "react-icons/io";
 
-const SideBar = () => {
+const SideBar = ({ toggleSidebar, isSidebarOpen }) => {
   return (
-    <aside className="h-screen w-64 bg-primary-pink text-white fixed top-0 left-0 transition-all duration-300">
-      <nav className="flex flex-col h-full p-4">
+    <aside className="md:static  fixed top-0 bottom-0 left-0 z-50 md:w-64 sm:w-1/2 w-3/4 bg-primary-pink h-full text-white transition-all duration-300">
+      <nav className=" relative flex flex-col  p-4 md:items-start items-center h-screen">
         {/* Logo Section */}
-        <div className="flex items-center mb-6">
-          <BiBook className="text-3xl mr-2" /> {/* Updated to a book icon for clinic logo */}
-          <h2 className="text-xl font-semibold">Laib Clinic</h2>
+        <div className='flex justify-center items-center w-full'>
+          <div className="flex items-center mb-6">
+            <BiBook className="text-3xl mr-2" /> {/* Updated to a book icon for clinic logo */}
+            <h2 className="text-xl font-semibold">Laib Clinic</h2>
+          </div>
+          {isSidebarOpen && <IoMdCloseCircle onClick={toggleSidebar} className='md:hidden absolute right-4 top-4 w-6 h-6'  color='white'/>}
         </div>
+        
 
         {/* Menu List */}
         <ul className="flex-1 space-y-4">
           {/* Dashboard */}
           <li>
-            <Link to="/" className="flex items-center p-2 hover:bg-white hover:text-primary-pink rounded transition">
+            <Link to="/"  onClick={() => {
+                                        if (window.innerWidth < 768) {
+                                            toggleSidebar();
+                                        }}}
+                                        className="flex items-center p-2 hover:bg-white hover:text-primary-pink rounded transition">
               <BiHome className="mr-2" />
               <span className="font-semibold">Dashboard</span>
             </Link>
           </li>
           {/* Appointments */}
           <li>
-            <Link to="/appointments" className="flex items-center p-2 hover:bg-white hover:text-primary-pink rounded transition">
+            <Link to="/appointments" onClick={() => {
+                                        if (window.innerWidth < 768) {
+                                            toggleSidebar();
+                                        }}}
+                                       className="flex items-center p-2 hover:bg-white hover:text-primary-pink rounded transition">
               <BiCalendar className="mr-2" />
               <span className="font-semibold">Appointments</span>
             </Link>
           </li>
           {/* Orders */}
           <li>
-            <Link to="/orders" className="flex items-center p-2 hover:bg-white hover:text-primary-pink rounded transition">
+            <Link to="/orders"  onClick={() => {
+                                        if (window.innerWidth < 768) {
+                                            toggleSidebar();
+                                        }}}
+                                        className="flex items-center p-2 hover:bg-white hover:text-primary-pink rounded transition">
               <BiShoppingBag className="mr-2" />
               <span className="font-semibold">Orders</span>
             </Link>
           </li>
           {/* Courses */}
           <li>
-            <Link to="/courses" className="flex items-center p-2 hover:bg-white hover:text-primary-pink rounded transition">
+            <Link to="/courses" onClick={() => {
+                                        if (window.innerWidth < 768) {
+                                            toggleSidebar();
+                                        }}}
+                                         className="flex items-center p-2 hover:bg-white hover:text-primary-pink rounded transition">
               <BiBook className="mr-2" />
               <span className="font-semibold">Courses</span>
             </Link>
           </li>
           {/* Statistics */}
           <li>
-            <Link to="/statistics" className="flex items-center p-2 hover:bg-white hover:text-primary-pink rounded transition">
+            <Link to="/statistics"  onClick={() => {
+                                        if (window.innerWidth < 768) {
+                                            toggleSidebar();
+                                        }}}
+                                         className="flex items-center p-2 hover:bg-white hover:text-primary-pink rounded transition">
               <BiStats className="mr-2" />
               <span className="font-semibold">Statistics</span>
             </Link>
