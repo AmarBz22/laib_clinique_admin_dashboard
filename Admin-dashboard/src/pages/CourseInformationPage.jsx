8101c04b025d730654b87cb857dd9f438b741fb8
SifEddine05 <sifousamir72@gmail.com>
2024-10-15 14:43:05 +0100

    make the model responsive

--- Admin-dashboard/src/pages/CourseInformationPage.jsx
+++ Admin-dashboard/src/pages/CourseInformationPage.jsx
@@ -149,7 +149,7 @@ const CourseInformationPage = () => {
         </div>
         {isModalOpen && (
           <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-            <div className="bg-white rounded-lg p-6 w-1/3">
+            <div className="bg-white rounded-lg p-6  w-94">
               <h3 className="text-lg font-semibold mb-4">Delete Course</h3>
               <p className="mb-4">
                 Are you sure you want to delete this course?
